847e3a40d20a69764ae2bfd0fe23d6d98a134c56
Gopher1981 <gopher.williams@gmail.com>
2022-06-28 12:49:23 +0100

    28/06 before work

--- Python Projects/Config_print_app_mk1.py
+++ Python Projects/Config_print_app_mk1.py
@@ -1,3 +1,4 @@
+from email.policy import strict
 from tkinter import *
 import tkinter as tk
 from tkinter.ttk import Notebook
@@ -35,12 +36,21 @@ app_title = tk.Label(frametop, text="Config General Printing Application", font=
 
 tab1 = tk.Frame(frame2)
 tab2 = tk.Frame(frame2)
+tab2a = tk.Frame(tab2)
+tab2b = tk.Frame(tab2)
 tab3 = tk.Frame(frame2)
 tab4 = tk.Frame(frame2)
+tab5 = tk.Frame(frame2)
+tab6 = tk.Frame(frame2)
 frame2.add(tab1, text = "Singles")
 frame2.add(tab2, text = "Groups")
 frame2.add(tab3, text = "Range")
 frame2.add(tab4, text = "Range-Auto")
+frame2.add(tab5, text = "Other malarky")
+frame2.add(tab6, text = "Report low print stock")
+
+tab2a.pack(anchor=CENTER, expand=False, side=TOP, pady=10, padx=10)
+tab2b.pack(anchor=CENTER, expand=False, side=TOP, pady=10, padx=10)
 
 printer_select = tk.StringVar(None, "LPT1")
 tag_select = tk.IntVar(value=0)
@@ -88,8 +98,23 @@ def return_key(event = None):
     if tab_index == 3:
         print("Range-Auto")
 
+# ============ Command definitions ============
+
 def clear_group_text():
     group_textbox.delete("1.0", END)
+
+def print_group_text():
+    print(group_textbox.get("1.0", END))
+    group_textbox.delete("1.0", END)
+
+def clear_range():
+    range_entry2.delete()
+    range_entry3.delete()
+    range_entry4.delete()
+    range_entry5.delete()
+
+def print_range():
+    print("printing range")
 
 # ============ Settings panel (frame1a) ============
 
@@ -155,24 +180,109 @@ single_descript.grid(row=1, column=1, columnspan=2, sticky=N)
 
 # ============ Groups Tab (tab2) ============
 
-group_label = tk.Label(master=tab2,
+group_label = tk.Label(master=tab2a,
                         textvariable=asset_type)
-group_label.grid(row=0, column=1)
+group_label.pack(side=LEFT)
 
-group_entry = tk.Entry(master=tab2)
-group_entry.grid(row=0, column=2)
+group_entry = tk.Entry(master=tab2a)
+group_entry.pack(side=LEFT, padx=10)
 
-group_clear = tk.Button(master=tab2,
+group_clear = tk.Button(master=tab2b,
                         text="Clear",
                         command=clear_group_text)
-group_clear.grid(row=1, columnspan=4)
+group_clear.pack(side=LEFT)
+
+group_print = tk.Button(master=tab2b,
+                        text="Print",
+                        bg="grey80",
+                        command=print_group_text)
+group_print.pack(side=LEFT, padx=100)
 
 group_textbox = Text(master=tab2, wrap=WORD)
-group_textbox.grid(row=2, columnspan=4, rowspan=2, sticky=NSEW)
+group_textbox.pack(side=BOTTOM, fill=BOTH, expand=True, padx=5, pady=5)
 
 # ============ Range Tab (tab3) ============
 
+range_label1 = tk.Label(master=tab3,
+                        text="For printing a range of asset tags")
+range_label1.pack()
+
+range_label2 = tk.Label(master=tab3,
+                        text="Enter the Prefix of the tag")
+range_label2.pack()
+
+range_entry2 = tk.Entry(master=tab3)
+range_entry2.pack()
+
+range_label3 = tk.Label(master=tab3,
+                        text="Enter the Suffix of the tag")
+range_label3.pack()
+
+range_entry3 = tk.Entry(master=tab3)
+range_entry3.pack()
+
+range_label4 = tk.Label(master=tab3,
+                        text="Enter the start number of the range")
+range_label4.pack()
+
+range_entry4 = tk.Spinbox(master=tab3,
+                        from_=0, to=99999999999999999)
+range_entry4.pack()
+
+range_label5 = tk.Label(master=tab3,
+                        text="Enter the end number of the range")
+range_label5.pack()
+
+range_entry5 = tk.Spinbox(master=tab3,
+                        from_=0, to=99999999999999999)
+range_entry5.pack()
+
+range_clear = tk.Button(master=tab3,
+                        text="Clear",
+                        command=clear_range)
+range_clear.pack(side=LEFT)
+
+range_print = tk.Button(master=tab3,
+                        text="Print",
+                        bg="grey80",
+                        command=print_range)
+range_print.pack(side=LEFT, padx=100)
+
 # ============ Range-Auto Tab (tab4) ============
+
+# ============ Malarky Tab (tab5) ============
+
+print_quantity = tk.Spinbox(master=tab5, from_=0, to=9999)
+print_quantity.pack(padx=5, pady=5)
+
+bbc_button = tk.Button(master=tab5,
+                        text="BBC")
+bbc_button.pack(padx=5, pady=5)
+
+bbc_button = tk.Button(master=tab5,
+                        text="UOB Mac QR Code")
+bbc_button.pack(padx=5, pady=5)
+
+bbc_button = tk.Button(master=tab5,
+                        text="UOB PC QR Code")
+bbc_button.pack(padx=5, pady=5)
+
+# ============ Report Tab (tab6) ============
+
+label1 = tk.Label(master=tab6,
+                    text="How many rolls of labels remain?")
+label1.pack(pady=5)
+
+labels_remain = tk.Spinbox(master=tab6, from_=0, to=5, wrap=True)
+labels_remain.pack(pady=5)
+
+label_send = tk.Button(master=tab6,
+                        text="Alert Admins")
+label_send.pack(pady=5)
+
+label2 = tk.Label(master=tab6,
+                    text="More than 5 packs is plenty and doesn't need to be reported")
+label2.pack(pady=5)
 
 # ============ Start up the routine ============
 
