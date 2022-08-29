afa78fca221986f59404bb06715264c734c7af59
Gopher1981 <gopher.williams@gmail.com>
2022-08-29 13:27:38 +0100

    xml1

---
+++ Python Projects/xmlButtons/test1.py
@@ -0,0 +1,14 @@
+########################################################
+# The purpose of this is to test importing an xml file #
+# and generating clickable buttons along with commands #
+# and definitions associated with said button          #
+########################################################
+
+import tkinter as tk
+from tkinter import *
+from configparser import ConfigParser
+
+root = tk.Tk()
+root.title("XML Import Test")
+
+root.mainloop()
