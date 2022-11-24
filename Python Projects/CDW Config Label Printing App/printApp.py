0193679a435ddcbe55865efe3a5392902e9d04a8
Dave Williams <108024405+Dave-Willi@users.noreply.github.com>
2022-11-24 14:44:01 +0000

    Update printApp.py

--- Python Projects/CDW Config Label Printing App/printApp.py
+++ Python Projects/CDW Config Label Printing App/printApp.py
@@ -17,7 +17,6 @@ from tkinter import ttk
 from tkinter import *
 from tkinter import messagebox, filedialog
 from tkinter.ttk import Notebook, Style
-from turtle import width
 from PIL import Image, ImageTk
 from configparser import ConfigParser
 import tkinter.scrolledtext as tkscrolled
@@ -113,6 +112,7 @@ frame2.pack(padx=10,pady=10, anchor=E, fill=BOTH, expand=True, side=RIGHT)
 # Creates a style for use on the notebook tabs
 Mysky = "#DCF0F2"
 Myyellow = "#F2C88B"
+MyGrey = "#FAFAFA"
 style = Style()
 style.theme_create( "dummy", parent="alt", settings={
         "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0] } },
@@ -120,7 +120,8 @@ style.theme_create( "dummy", parent="alt", settings={
             "configure": {"padding": [5, 1], "background": Mysky,
                             "font":("aerial",12)},
             "map":       {"background": [("selected", Myyellow)],
-                          "expand": [("selected", [1, 1, 1, 0])], } } } )
+                        # "background": [("disabled", MyGrey)],
+                          "expand": [("selected", [1, 1, 1, 0])],} } } )
 style.theme_use("dummy")
 
 
@@ -609,6 +610,61 @@ except:
 # # ==========================================
 # # ================= Classes ================
 # # ==========================================
+
+class CreateToolTip(object):
+    """
+    create a tooltip for a given widget
+    """
+    def __init__(self, widget, text='widget info'):
+        self.waittime = 500     #miliseconds
+        self.wraplength = 180   #pixels
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
+
+    def enter(self, event=None):
+        self.schedule()
+
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
+
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 20
+        # creates a toplevel window
+        self.tw = tk.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                       background="#ffffff", relief='solid', borderwidth=1,
+                       wraplength = self.wraplength)
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tw
+        self.tw= None
+        if tw:
+            tw.destroy()
+
+
 
 class Dominos(tk.Tk):
     # enter store number
@@ -1782,6 +1838,16 @@ File1 = open(file_list, "w")
 File1.write(files)
 # Step 5, close file
 File1.close()
+
+# ===============================
+# ========== Tooltips ===========
+# ===============================
+
+from idlelib.tooltip import Hovertip
+    
+myTip = Hovertip(tab3,'This is \na multiline tooltip.')
+
+
 
 # ===============================
 # ========= Dev Tools ===========
