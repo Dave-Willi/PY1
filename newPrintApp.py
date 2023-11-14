5960f5cfc6433bb5dae2f673f6e8847d5784bb74
Dave Williams <108024405+Dave-Willi@users.noreply.github.com>
2023-11-14 17:12:38 +0000

    1

--- newPrintApp.py
+++ newPrintApp.py
@@ -11,10 +11,13 @@
 ###################
 
 import tkinter as tk
-# import tkinter.scrolledtext as tkscrolled
-
+import tkinter.scrolledtext as tkscrolled
 # from PrintAppFunctions import rangeToList
-# from functools import partial
+from functools import partial
+from tkinter import messagebox
+import re
+
+
 ########################
 # Initialising Program #
 ########################
@@ -37,21 +40,87 @@ root.geometry('%dx%d+%d+%d' % (appWidth, appHeight, x, y))
 root.resizable(False,False) # Disable resizing of window
 
 
-# rangeLabel = tk.Label(text="Range test")
-# rangeLabel.pack()
 
-# rangeInput1 = tk.Entry()
-# rangeInput1.pack()
 
-# rangeInput2 = tk.Entry()
-# rangeInput2.pack()
 
-# rangeBtn = tk.Button(command=rangeToList(rangeInput1.get(),rangeInput2.get())
-#                     ,text="Print")
-# rangeBtn.pack()
 
-# rangeList = tkscrolled.ScrolledText(height=12)
-# rangeList.pack()
+def rangeToList(range1,range2): # To take 2 points of a range and create a list going from one to the other
+    print("This is not running")
+    print(range1)
+    print(range2)
+    if range1 == "" or range2 =="" or len(range1) != len(range2): # error out of function if one or both range values are empty or they don't match length
+        return -1
+    rangePrefix1 = ""
+    rangeStart = ""
+    rangeSuffix1 = "" 
+    rangePrefix2 = ""
+    rangeEnd = ""
+    rangeSuffix2 = ""
+    rangeSplit1 = re.split("(\d+)", range1)
+    rangeSplit2 = re.split("(\d+)", range2)
+    y = 0
+    z = 0
+    for x in rangeSplit1: #cycles through however many splits exist in the first split
+        if rangeSplit1[y] != rangeSplit2[y]:
+            z = y + 1
+            rangeStart = min(rangeSplit1[y],rangeSplit2[y]) # sets the lowest number entered into the start
+            rangeEnd = max(rangeSplit2[y],rangeSplit1[y]) # sets the highest number entered into the end
+            for x in rangeSplit1[z:]:
+                try:
+                    if rangeSplit1[z] == rangeSplit2[z]:
+                        rangeSuffix1 += x
+                        rangeSuffix2 += x
+                        z+=1
+                    else:
+                        messagebox.showerror("Error", "Problem determining the suffix")
+                        return
+                except:
+                    break
+            break
+        elif rangeSplit1[y] == rangeSplit2[y]:
+            rangePrefix1 += x
+            rangePrefix2 += x
+        y+=1
+    if rangePrefix1 != rangePrefix2:
+        messagebox.showerror("Error", "Error detected in the prefix. \nPlease check and try again")
+        return
+    elif rangeSuffix1 != rangeSuffix2:
+        messagebox.showerror("Error", "Error detected in the suffix. \nPlease check and try again")
+        return
+    else:
+            lead_zeros = len(rangeEnd)
+            prefixed = str(rangePrefix1).upper()
+            suffixed = str(rangeSuffix1).upper()
+            newlist = ""
+            for x in range(int(rangeStart), int(rangeEnd)+1):
+                y = str(x).zfill(lead_zeros)
+                newlist += prefixed + y + suffixed
+            # addToList(newlist)
+            print(newlist)
+            # clearInputs()
+    return
+
+
+
+
+
+
+
+rangeLabel = tk.Label(text="Range test")
+rangeLabel.pack()
+
+rangeInput1 = tk.Entry()
+rangeInput1.pack()
+
+rangeInput2 = tk.Entry()
+rangeInput2.pack()
+
+rangeBtn = tk.Button(command=rangeToList(rangeInput1.get(),rangeInput2.get())
+                    ,text="Print")
+rangeBtn.pack()
+
+rangeList = tkscrolled.ScrolledText(height=12)
+rangeList.pack()
 
 
 #################
